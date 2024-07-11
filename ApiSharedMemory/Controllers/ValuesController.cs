add7465bf5f255b65f105f2c6f00ebbb40c5dbe1
NDIAYEFATAH <fatahndiaye234@gmail.com>
2024-07-11 17:52:30 +0000

    commit

---
+++ ApiSharedMemory/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using MetierSharedMemory.Model;
+
+namespace ApiSharedMemory.Controllers
+{
+
+    [RoutePrefix("api")]
+    public class ValuesController : ApiController
+    {
+        /*private dbKireneEntities7 db = new dbKireneEntities7();*/
+        BdSharedMemoryContext dbs = new BdSharedMemoryContext();
+        private readonly BdSharedMemoryContext _context;
+
+        // Constructeur par défaut sans paramètres
+        public ValuesController()
+        {
+            _context = new BdSharedMemoryContext();
+        }
+
+        // Constructeur avec paramètre (si nécessaire pour des tests ou une injection de dépendances)
+        public ValuesController(BdSharedMemoryContext context)
+        {
+            _context = context ?? new BdSharedMemoryContext();
+        }
+
+        
+        // GET api/values
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Jury> Get()
+        {
+            return dbs.Jurys;
+        }
+
+        [HttpGet]
+        [Route("GetListJury")]
+        public List<Jury> GetListJury()
+        {
+            return dbs.Jurys.ToList();
+
+        }
+
+        // GET api/values/5
+        public string Get(int id) 
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- appsharedMemory/Form1.cs
+++ appsharedMemory/Form1.cs
@@ -4,27 +4,58 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using appsharedMemory.ServiceMetier;
+using MetierSharedMemory;
+using Newtonsoft.Json;
 
 namespace appsharedMemory
 {
     public partial class Form1 : Form
     {
-        ServiceMetier.Service1Client service = new ServiceMetier.Service1Client();
+        Service1Client serv;
+        /*ServiceMetier.Service1Client service = new ServiceMetier.Service1Client();*/
         public Form1()
         {
-            service = new ServiceMetier.Service1Client();
+            /*service = new ServiceMetier.Service1Client();*/
+            serv = new Service1Client("BasicHttpBinding_IService1");
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dgJury.DataSource = service.GetJurys();
+            try
+            {
+                dgJury.DataSource = servGetListJury();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur : {ex.Message}");
+            }
+            
+            /*dgJury.DataSource = servGetListJury();*/
         }
+        public List<Jury> servGetListJury()
+        {
+            HttpClient client;
+            client = new HttpClient();
+            var services = new List<Jury>();
+            client.BaseAddress = new Uri(System.Configuration.ConfigurationSettings.AppSettings["ServeurApiUrl"]);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = client.GetAsync("appShared/values/GetListJury").Result;
 
+            if (response.IsSuccessStatusCode)
+            {
+                var responseData = response.Content.ReadAsStringAsync().Result;
+                services = JsonConvert.DeserializeObject<List<Jury>>(responseData);
+            }
+            return services;
+        }
         private void btnAjouter_Click(object sender, EventArgs e)
         {
             ServiceMetier.Jury jury = new ServiceMetier.Jury();
@@ -32,7 +63,7 @@ namespace appsharedMemory
             jury.Nom = txtNom.Text;
             jury.Grade = txtGrade.Text;
             jury.Specialite = txtSpecialite.Text;
-            service.AddJury(jury);
+            serv.AddJury(jury);
             Effacer();
         }
 
@@ -42,7 +73,7 @@ namespace appsharedMemory
             txtPrenom.Text = string.Empty;
             txtGrade.Text = string.Empty;
             txtSpecialite.Text = string.Empty;
-            dgJury.DataSource = service.GetJurys();
+            dgJury.DataSource = serv.GetJurys();
             txtNom.Focus();
         }
     }
